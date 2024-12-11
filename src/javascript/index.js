ad90fda9df4c6fd668b1c14deab54517a5e59e8f
curso12 <curso12@curso12>
2024-12-11 13:48:28 +0100

    add button

---
+++ src/javascript/index.js
@@ -0,0 +1,11 @@
+$(function) {
+    const data = [
+        { name: 'First', surname: 'Second' },
+        { name: 'Primero', surname: 'Secundo'}
+    ];
+}
+
+$(function loadTable()) {
+    const tableBody = document.getElementById('tableBody');
+    
+}
